a1a2ce8dff727a49296671cffa74e2dda172af56
Peterelzevir <114345532+Peterelzevir@users.noreply.github.com>
2025-03-02 09:22:48 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'standalone', // Tambahkan ini untuk Netlify
   images: {
     domains: ['api.ryzendesu.vip'],
   },
 };
-
 module.exports = nextConfig;
